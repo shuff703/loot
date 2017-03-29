9457380bf5d10c1a2ceb08ae1d4e514e02d76ec2
Christopher Stump <cstump1@uncc.edu>
2017-03-29 02:34:55 +0000

    Added the face of the sign up page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,8 @@
 class AccountsController < ApplicationController
     def index
-<<<<<<< HEAD
-=======
         @accounts = Account.all
     end
     def new
         @account = Account.new
->>>>>>> 3ab38ddbbb986940b4bdb5ebf5c7fbd50fc307d9
     end
 end
