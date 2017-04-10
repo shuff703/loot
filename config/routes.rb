e0951b80facf17276f51d10af465ad4f23e7a628
Scott Huff <shuff8@uncc.edu>
2017-04-10 14:37:25 +0000

    Re-Added Transactions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   resources :accounts
   
   resources :budgets do
+    resources :transactions
     end
   
   get 'accounts/index'
-  #get 'budgets/index'
-  #post 'budgets/create'
   get 'signup' => 'accounts#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
