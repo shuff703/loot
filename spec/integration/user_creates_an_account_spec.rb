ada6542be240c020e785b3be6049bd2a51de84c1
Christopher Stump <cstump1@uncc.edu>
2017-04-29 18:15:25 +0000

    Added a testing scenario for creating transactions

--- spec/integration/user_creates_an_account_spec.rb
+++ spec/integration/user_creates_an_account_spec.rb
@@ -16,7 +16,7 @@ feature "User can create an account and log in" do
         expect(page).to have_content("Budgets")
     end
     
-    scenario "Creates account and logs in with incorrect credentials" do
+    scenario "User should not be able to log in with incorrect credentials" do
         visit '/'
         expect(page).to have_content("Log In")
         click_link('Sign Up')
@@ -33,6 +33,8 @@ feature "User can create an account and log in" do
     end
     
 end
+
+
 
 feature "User should be able to create budgets" do
     scenario "Creates a budget" do
@@ -59,4 +61,48 @@ feature "User should be able to create budgets" do
         
         
     end
+    
+end
+
+
+
+feature "User should be able to create transactions" do
+    scenario "Creates a transaction" do
+        visit '/'
+        expect(page).to have_content("Log In")
+        click_link('Sign Up')
+        expect(page).to have_content("Join Loot Today!")
+        fill_in('account[username]', :with => 'Test')
+        fill_in('account[password]', :with => 'password')
+        click_button("Create Account")
+        expect(page).to have_content("Log In")
+        fill_in('Username', :with => 'Test')
+        fill_in('Password', :with => 'password')
+        click_button("Log In")
+        expect(page).to have_content("Budgets")
+        click_link("New Budget")
+        expect(page).to have_content("New Budget")
+    
+        fill_in "Name", :with => 'Budget 2017'
+        fill_in "Limit", :with => '1500.00'
+        
+        click_button("Create Budget")
+        expect(page).to have_content("Budget 2017")
+        
+        expect(page).to have_content("Add Transaction")
+        
+        click_link("Add Transaction")
+        expect(page).to have_content("New Transaction")
+        
+        fill_in "transaction[recipient]", :with => 'Starbucks'
+        fill_in "transaction[amount]", :with => '20.00'
+        select "Food", :from => "transaction[category]"
+        fill_in "transaction[date]", :with => '04/27/2017'
+        
+        click_button("Create Transaction")
+        
+        expect(page).to have_content("Starbucks 20.0")
+        
+    end
+    
 end
