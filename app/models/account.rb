7efb16187ffafe97f8a0d0c92174accb46077c35
Scott Huff <shuff8@uncc.edu>
2017-04-30 20:58:42 +0000

    Added model validation and error messages, probably a byebug in there too

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ApplicationRecord
     has_secure_password
     has_many :budgets, dependent: :destroy
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 8 }
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :budget
+  validates :recipient, presence: true
+  validates :amount, presence: true
+  validates_numericality_of :amount, greater_than: 0
+  validates :category, presence: true
+  validates :date, presence: true
 end
